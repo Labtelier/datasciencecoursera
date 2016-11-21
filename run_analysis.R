b202de64cf914b3900aecfe18d511a128b2c7d4d
Benoit de TARADE <benoit.de.tarade@gmail.com>
2016-11-21 19:24:30 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,11 @@
+##BEWARE!!!!!
+##WHat is requested is :
+##a tidy dataset AND NOT a file named tiny or tidy
+##the tidy dataset must contains the mean group by
+##-each variable
+##-each activity
+##-AND each subject
+
 ##1. Merges the training and the test sets to create one data set.
 ##2. Extracts only the measurements on the mean and standard deviation for each measurement.
 ##3. Uses descriptive activity names to name the activities in the data set
@@ -42,30 +50,12 @@ cleanFeatures  <- gsub("[()]", "", features[,2])
 subject_test <- read.table("test/subject_test.txt")
 X_test <- read.table("test/X_test.txt")
 y_test <- read.table("test/y_test.txt")
-body_acc_x_test <- read.table("test/Inertial Signals/body_acc_x_test.txt")
-body_acc_y_test <- read.table("test/Inertial Signals/body_acc_y_test.txt")
-body_acc_z_test <- read.table("test/Inertial Signals/body_acc_z_test.txt")
-body_gyro_x_test <- read.table("test/Inertial Signals/body_gyro_x_test.txt")
-body_gyro_y_test <- read.table("test/Inertial Signals/body_gyro_y_test.txt")
-body_gyro_z_test <- read.table("test/Inertial Signals/body_gyro_z_test.txt")
-total_acc_x_test <- read.table("test/Inertial Signals/total_acc_x_test.txt")
-total_acc_y_test <- read.table("test/Inertial Signals/total_acc_y_test.txt")
-total_acc_z_test <- read.table("test/Inertial Signals/total_acc_z_test.txt")
 
 
 ##Read train data
 subject_train <- read.table("train/subject_train.txt")
 X_train <- read.table("train/X_train.txt")
 y_train <- read.table("train/y_train.txt")
-body_acc_x_train <- read.table("train/Inertial Signals/body_acc_x_train.txt")
-body_acc_y_train <- read.table("train/Inertial Signals/body_acc_y_train.txt")
-body_acc_z_train <- read.table("train/Inertial Signals/body_acc_z_train.txt")
-body_gyro_x_train <- read.table("train/Inertial Signals/body_gyro_x_train.txt")
-body_gyro_y_train <- read.table("train/Inertial Signals/body_gyro_y_train.txt")
-body_gyro_z_train <- read.table("train/Inertial Signals/body_gyro_z_train.txt")
-total_acc_x_train <- read.table("train/Inertial Signals/total_acc_x_train.txt")
-total_acc_y_train <- read.table("train/Inertial Signals/total_acc_y_train.txt")
-total_acc_z_train <- read.table("train/Inertial Signals/total_acc_z_train.txt")
 
 
 ##Apply features labels to x_train and x_test columns name
@@ -117,5 +107,5 @@ subjects.summary$activityId <- NULL
 subject.df <- as.data.frame(subjects.summary)
 
 #Store data in a flat file
-write.csv(subject.df, file = "subject.summary.csv", row.names = FALSE)
+write.csv(subject.df, file = "tidy.txt", row.names = FALSE)
 
